9cb20ec5afd468db3c499757b8523885f4f72daf
Tabishhilal996 <127969534+Tabishhilal996@users.noreply.github.com>
2023-04-02 13:26:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+alert("Ramzan mubarak");
+
+// console.log("Ramzan or javascript");
+
+// var a = 225;
+// var b = 200;
+// var c = a + b;
+// console.log(c)
+
+// var a = 10;
+
+// var b = a++;
+// console.log(b)
+// console.log(a)
+
+// var firstName = prompt("enter first name");
+// var lastName = prompt("enter last name");
+// var city = prompt("enter city", "Karachi");
+// console.log(firstName + " " + lastName + " from " + city);
+
+
+var mithaimilrahihai = true
+
+
+if(mithaimilrahihai == true){console.log("ajao");} else{console.log("dusri masjid");}
